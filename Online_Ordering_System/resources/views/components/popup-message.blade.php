83dfc1bc8a79b891b87014a4bab94a783b0bbf8e
Joe Cin Ng <75717177+joecinng@users.noreply.github.com>
2024-06-18 14:00:15 +1000

    Delete Online_Ordering_System directory

--- Online_Ordering_System/resources/views/components/popup-message.blade.php
+++
@@ -1,7 +0,0 @@
-@if (session()->has('message')) 
-    <div>
-        <p class="popup-message">
-            {{session('message')}}
-        </p>
-    </div>
-@endif
--- Online_Ordering_System/resources/views/partials/_search.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="">
-    <div class="row my-4 w-100">
-        <input type="text" name="search" class="mx-3 col-6 p-4 fw-bold form-control" placeholder="Search for an item...">
-        <button class="col-1 ms-3 search-btn btn btn-outline-primary" type="submit"><i class="fa fa-search" class="align-self-center" aria-hidden="true"></i></button>
-       <div class="col-2"></div>
-    </div>
-</form>
